07504cbb04b8086370346ac9d913e0beeb1a9204
wangtao <lionel_tao@163.com>
2024-01-25 21:13:59 +0800

    feat:更新

--- simple-vite.js
+++ simple-vite.js
@@ -4,7 +4,16 @@ const path = require('path')
 const { URL } = require('url')
 const compilerSfc = require('@vue/compiler-sfc')
 const compilerDom = require('@vue/compiler-dom')
+const basePath = 'src'
 
+
+// 检查是否是静态文件
+const imageRE = /\.(png|jpe?g|gif|svg|ico|webp)(\?.*)?$/;
+const mediaRE = /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/;
+const fontsRE = /\.(woff2?|eot|ttf|otf)(\?.*)?$/i;
+const isStaticAsset = (file) => {
+  return imageRE.test(file) || mediaRE.test(file) || fontsRE.test(file);
+};
 
 function rewriteImport(content) {
   return content.replace(/ from ['|"]([^'"]+)['|"]/g, function (s0, s1) { // 找到 from 'vue'
@@ -16,10 +25,21 @@ function rewriteImport(content) {
   })
 }
 
+function writeFileCss(content) {
+  return `const css = "${content}"
+  let link = document.createElement('style')
+  link.setAttribute('type','text/css')
+  document.head.appendChild(link)
+  link.innerHTML = css
+  export default css
+`
+}
+
 const server = http.createServer((req, res) => {
   const { url } = req
   const query = new URL(req.url, `http://${req.headers.host}`).searchParams
 
+  // 拦截html
   if (url === '/') {
     // 设置Content-type 响应头是为了让浏览器已html的编码方式去加载这份资源
     res.writeHead(200, {
@@ -46,19 +66,25 @@ const server = http.createServer((req, res) => {
       'Content-type': 'application/javascript'
     })
     res.end(rewriteImport(content))
-  } else if (url.indexOf('vue') !== -1) {
+  } else if (url.indexOf('.vue') !== -1) {
     const p = path.resolve(__dirname, url.split('?')[0].slice(1))
     const { descriptor } = compilerSfc.parse(fs.readFileSync(p, 'utf8'))
     if (!query.get('type')) {
       res.writeHead(200, {
         'Content-type': 'application/javascript'
       })
-      const content = `
+      let content = `
         ${rewriteImport(descriptor.script.content.replace('export default', 'const __script='))}
         import {render as __render} from "${url}?type=template"
         __script.render = __render
         export default __script
       `
+      if (descriptor.styles) {
+        descriptor.styles.forEach((s, i) => {
+          content += `import '${url}?type=style&index=${i}'`
+        });
+      }
+
       res.end(content)
     } else if (query.get('type') === 'template') { // 返回vue文件的html部分
       const template = descriptor.template
@@ -67,26 +93,40 @@ const server = http.createServer((req, res) => {
         'Content-type': 'application/javascript'
       })
       res.end(rewriteImport(render))
-
+    } else if (query.get('type') === 'style') { // 处理vue文件的html 部分 
+      let index = query.get('index')
+      let styleContent = descriptor.styles[index].content
+      const content = writeFileCss(styleContent.replace(/\r\n/g, ''))
+      res.writeHead(200, {
+        'Content-type': 'application/javascript'
+      })
+      res.end(content)
     }
+
+
   } else if (url.endsWith('.css')) {
     const p = path.resolve(__dirname, url.slice(1))
     const file = fs.readFileSync(p, 'utf8')
-    const content = `
-      const css = "${file.replace(/\n/g, '')}"
-      let link = document.createElement('style')
-      link.setAttribute('type','text/css')
-      document.head.appendChild(link)
-      link.innerHTML = css
-      export default css
-    `
+    const content = writeFileCss(file.replace(/\r\n/g, ''))
     res.writeHead(200, {
       'Content-type': 'application/javascript'
     })
     res.end(content)
+  } else if (isStaticAsset(url)) {
+    // res.writeHead(200, {
+    //   'Content-type': 'application/javascript'
+    // })
+    // console.log(process.cwd())
+    // console.log(basePath + url)
+    // const p = path.resolve(__dirname, url.slice(1))
+
+    res.end(`export default '/src/assets/vue.svg'`)
   }
+
+
 })
 
 server.listen(8080, () => {
+  console.log('http://localhost:8080/')
   console.log('listening on port 8080 ')
 })
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
-<template>
-  <div>
-    <h1>Hello Vite</h1>
-  </div>
-</template>
-
 <script lang="ts">
-import { defineComponent, reactive, toRefs, ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "",
-  components: {},
-  setup() {
-    return {};
+  components: {
+    HelloWorld,
   },
+  setup() {},
 };
 </script>
-<style lang="scss" scoped></style>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
